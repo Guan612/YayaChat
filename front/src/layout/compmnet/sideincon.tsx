fc9474927d46d2421816919f6eccc61434382d12
Guan612 <tiantony53@163.com>
2024-07-20 15:41:30 +0800

    基础组件

--- front/src/layout/compmnet/sideincon.tsx
+++ front/src/layout/compmnet/sideincon.tsx
@@ -1,5 +1,11 @@
+import Button from '@mui/material/Button';
+
 export default function SideIcon() {
     return(
-        <div>sideincon</div>
+        <div className='flex flex-col justify-center'>
+            <div>chat</div>
+            <div>pepele</div>
+            <Button variant="contained">Contained</Button>
+        </div>
     )
 }
--- front/src/layout/index.tsx
+++ front/src/layout/index.tsx
@@ -3,10 +3,10 @@ import SideIcon from "./compmnet/sideincon";
 function Layout() {
     return (
         <div className="flex flex-row h-screen overflow-hidden">
-            <div className="hidden md:block md:basis-1/4 flex-col bg-red-300">
+            <div className="hidden md:block md:basis-1/5 flex-col bg-red-300">
                 <SideIcon></SideIcon>
             </div>
-            <div className="flex-1 md:basis-3/4 bg-blue-300">内容区</div>
+            <div className="flex-1 md:basis-4/5 bg-blue-300">内容区</div>
         </div>
     );
 }
