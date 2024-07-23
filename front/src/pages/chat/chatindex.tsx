cfd5cad0109755871560b98ba5cbfaa542a890ef
Guan612 <71303298+Guan612@users.noreply.github.com>
2024-07-23 16:58:42 +0800

    构建聊天主界面

---
+++ front/src/pages/chat/chatindex.tsx
@@ -0,0 +1,17 @@
+import PhoneOutlinedIcon from '@mui/icons-material/PhoneOutlined';
+import VideocamOutlinedIcon from '@mui/icons-material/VideocamOutlined';
+import ConnectedTvOutlinedIcon from '@mui/icons-material/ConnectedTvOutlined';
+import MoreHorizOutlinedIcon from '@mui/icons-material/MoreHorizOutlined';
+
+export default function ChatIndex() {
+    return (
+        <div className="flex flex-col h-screen">
+            <div className="flex flex-row bg-cyan-500 h-16">
+                <div className="font-bold text-white">canary</div>
+                <div>
+                    
+                </div>
+            </div>
+        </div>
+    )
+}
