5e44a2e9ea72835433f13d3a294b3f7b70d1fc47
Guan612 <71303298+Guan612@users.noreply.github.com>
2024-07-23 16:58:58 +0800

    引入组件

--- front/src/pages/chat/index.tsx
+++ front/src/pages/chat/index.tsx
@@ -1,11 +1,15 @@
+import ChatList from "./compment/chatlist";
+import ChatIndex from "./chatindex";
+import Search from "./compment/search";
 export default function Chat() {
     return (
-        <div className="flex flex-col h-screen">
-            <div className="flex-1 overflow-y-auto">
-                {/* Chat messages go here */}聊天
+        <div className="flex flex-row h-screen">
+            <div className="w-1/5 bg-gray-100">
+                <Search/>
+                <ChatList/>
             </div>
-            <div className="flex-none">
-                {/* Chat input goes here */}
+            <div>
+                <ChatIndex/>
             </div>
         </div>
     )
