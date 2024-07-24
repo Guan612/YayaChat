3e225b6c7a721e0b6e0dbbe2755b0f845432917b
Guan612 <71303298+Guan612@users.noreply.github.com>
2024-07-24 15:07:32 +0800

    Update index.tsx

--- front/src/pages/chat/index.tsx
+++ front/src/pages/chat/index.tsx
@@ -4,11 +4,11 @@ import Search from "./compment/search";
 export default function Chat() {
     return (
         <div className="flex flex-row h-screen">
-            <div className="flex flex-col w-1/5 bg-gray-100">
+            <div className="hidden md:flex flex-col w-1/5 bg-gray-100">
                 <Search/>
                 <ChatList/>
             </div>
-            <div className="flex flex-col w-4/5">
+            <div className="w-full flex flex-col md:w-4/5">
                 <ChatIndex/>
             </div>
         </div>
