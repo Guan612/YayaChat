1752b32a9883b2044e87fd731e6fd25acf4bb0bb
Guan612 <71303298+Guan612@users.noreply.github.com>
2024-07-24 10:19:35 +0800

    调整占比

--- front/src/pages/chat/index.tsx
+++ front/src/pages/chat/index.tsx
@@ -4,11 +4,11 @@ import Search from "./compment/search";
 export default function Chat() {
     return (
         <div className="flex flex-row h-screen">
-            <div className="w-1/5 bg-gray-100">
+            <div className="flex flex-col w-1/5 bg-gray-100">
                 <Search/>
                 <ChatList/>
             </div>
-            <div>
+            <div className="flex flex-col w-4/5">
                 <ChatIndex/>
             </div>
         </div>
