35eba2049d64bf8f445f436561ffbead1b56c8d2
Guan612 <71303298+Guan612@users.noreply.github.com>
2024-07-24 10:18:02 +0800

    完成聊天界面样式

--- front/src/pages/chat/chatindex.tsx
+++ front/src/pages/chat/chatindex.tsx
@@ -1,17 +1,29 @@
-import PhoneOutlinedIcon from '@mui/icons-material/PhoneOutlined';
-import VideocamOutlinedIcon from '@mui/icons-material/VideocamOutlined';
-import ConnectedTvOutlinedIcon from '@mui/icons-material/ConnectedTvOutlined';
-import MoreHorizOutlinedIcon from '@mui/icons-material/MoreHorizOutlined';
+import PhoneOutlinedIcon from "@mui/icons-material/PhoneOutlined";
+import VideocamOutlinedIcon from "@mui/icons-material/VideocamOutlined";
+import ConnectedTvOutlinedIcon from "@mui/icons-material/ConnectedTvOutlined";
+import MoreHorizOutlinedIcon from "@mui/icons-material/MoreHorizOutlined";
+import Badge from '@mui/material/Badge';
 
 export default function ChatIndex() {
     return (
         <div className="flex flex-col h-screen">
-            <div className="flex flex-row bg-cyan-500 h-16">
-                <div className="font-bold text-white">canary</div>
-                <div>
-                    
+            <div className="flex flex-row w-full bg-cyan-400 h-16 items-center">
+                <div className="flex font-bold text-white text-2xl items-center p-2">
+                    canary
+                </div>
+                <div className="flex-grow"></div>
+                <div className="flex">
+                    <PhoneOutlinedIcon fontSize="large" className="m-2 hover:bg-stone-500 rounded-lg"/>
+                    <VideocamOutlinedIcon fontSize="large" className="m-2 hover:bg-stone-500 rounded-lg"/>
+                    <ConnectedTvOutlinedIcon fontSize="large" className="m-2 hover:bg-stone-500 rounded-lg"/>
+                    <MoreHorizOutlinedIcon fontSize="large" className="m-2 hover:bg-stone-500 rounded-lg"/>
                 </div>
             </div>
+            <div className="flex flex-col flex-grow">聊天区域</div>
+            <div className="flex bg-cyan-400 h-32">
+                <input className="flex-grow m-2 rounded-lg" placeholder="输入消息..."></input>
+                <button className="bg-stone-500 m-2 rounded-lg">发送</button>
+            </div>
         </div>
-    )
+    );
 }
